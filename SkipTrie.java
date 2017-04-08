f0ebe1674b656dd8a21d216ac038e90b7427ad39
Joe <joeleav1@gmail.com>
2017-04-08 13:46:14 -0400

    SkipTrie skeleton

--- SkipTrie.java
+++ SkipTrie.java
@@ -3,8 +3,40 @@
  */
 
 import java.util.*;
-import java.util.concurrent.ConcurrentSkipListMap;
 
 public class SkipTrie {
+    private XfastTrie top;
+    private ConcurrentSkipListMap bottom;
 
+    public SkipTrie(){
+        top = new XfastTrie();
+        bottom = new ConcurrentSkipListMap();
+    }
+
+    public void insert(int value){
+        /*
+            Insert into skiplist first
+
+            If top level then
+                Insert into x-fast
+
+            **** Still need the coin flipping in the skip list
+         */
+    }
+
+    public void findPred(int x){
+        /*
+            Find pred in x-fast first
+            then find in the skip-list
+         */
+    }
+
+    public void delete(int x){
+        /*
+            First delete from skiplist
+
+            If it was a top level node then
+                delete from x-fast trie as well
+         */
+    }
 }
